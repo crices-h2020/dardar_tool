219a5bc230f38ce1b3b38b43136097b0870f7808
tappipro <arttu.vaisanen92@gmail.com>
2025-07-01 14:16:54 +0300

    some auto_download functionalities fixed

--- dardar_tool/main/validation_data_creator/validation_data_creator_main.py
+++ dardar_tool/main/validation_data_creator/validation_data_creator_main.py
@@ -317,7 +317,7 @@ def print_message(message1, message2, print_on):
 # ########################################################################
 
 
-def raw_validation_data_creator(start_date, end_date, lat_1, lat_2, lon_1, lon_2, variable_name, vertical_profile=None, exclude_top_on=True, exclude_bottom_on=True, verbose=1, auto_load_missing_files=True, version="V30", save_results=False, save_results_name="cloud_data", raw_data_loc=default_dardar_path):
+def raw_validation_data_creator(start_date, end_date, lat_1, lat_2, lon_1, lon_2, variable_name, vertical_profile=None, exclude_top_on=True, exclude_bottom_on=True, verbose=1, auto_load_missing_files=True, version="V30", save_results=False, save_results_name="cloud_data", raw_data_loc=default_dardar_path, key_location=""):
     if version == "V2":
         file_list = sorted(glob.glob(f"{raw_data_loc}/*.hdf"))
     else:
@@ -340,7 +340,7 @@ def raw_validation_data_creator(start_date, end_date, lat_1, lat_2, lon_1, lon_2
     if (missing_files_count > 0) and auto_load_missing_files:
         print("Files on your data folder didn't include all files that are relevant for chosen spatiotemporal location. \nDownloading missing files.")
         missing_files_list = check_for_missing_file(file_list, relevant_files_list)
-        load_functions.download_based_on_filenumber(missing_files_list, 0, raw_data_loc, version)
+        load_functions.download_based_on_filenumber(missing_files_list, 0, raw_data_loc, version, key_location)
 
     elif (missing_files_count > 0) and (auto_load_missing_files == False):
         print("Files on your data folder didn't include all files that are relevant for chosen spatiotemporal location.")
@@ -437,14 +437,14 @@ def raw_validation_data_creator(start_date, end_date, lat_1, lat_2, lon_1, lon_2
     return final_time, final_lat, final_lon, final_profile, vert_profile
 
 
-def extractor_plotting_tool(start_date, end_date, lat_1, lat_2, lon_1, lon_2, variable_name, vertical_profile, exclude_top_on=True, exclude_bottom_on=True, flight_separation_time=1, flight_separation_unit="m", colormap="viridis", verbose=1, auto_load_missing_files=True, version="V30", raw_data_loc=default_dardar_path):
+def extractor_plotting_tool(start_date, end_date, lat_1, lat_2, lon_1, lon_2, variable_name, vertical_profile, exclude_top_on=True, exclude_bottom_on=True, flight_separation_time=1, flight_separation_unit="m", colormap="viridis", verbose=1, auto_load_missing_files=True, version="V30", raw_data_loc=default_dardar_path, key_location=""):
     save_results=False
     save_results_name="" 
     profile_dardar = None
     print_message("Processing Custom Profile...\n", "Processing Custom Profile...\n", verbose)
-    tim1, lat1, lon1, user_profile_values, user_profile_alts = raw_validation_data_creator(start_date, end_date, lat_1, lat_2, lon_1, lon_2, variable_name, vertical_profile, exclude_top_on, exclude_bottom_on, verbose, auto_load_missing_files, version, save_results, save_results_name, raw_data_loc)
+    tim1, lat1, lon1, user_profile_values, user_profile_alts = raw_validation_data_creator(start_date, end_date, lat_1, lat_2, lon_1, lon_2, variable_name, vertical_profile, exclude_top_on, exclude_bottom_on, verbose, auto_load_missing_files, version, save_results, save_results_name, raw_data_loc, key_location=key_location)
     print_message("Processing dardar Profile...\n", "Processing dardar Profile...\n", verbose)
-    tim2, lat2, lon2, dardar_profile_values, dardar_profile_alts = raw_validation_data_creator(start_date, end_date, lat_1, lat_2, lon_1, lon_2, variable_name, profile_dardar, exclude_top_on, exclude_bottom_on, verbose, auto_load_missing_files, version, save_results, save_results_name, raw_data_loc)
+    tim2, lat2, lon2, dardar_profile_values, dardar_profile_alts = raw_validation_data_creator(start_date, end_date, lat_1, lat_2, lon_1, lon_2, variable_name, profile_dardar, exclude_top_on, exclude_bottom_on, verbose, auto_load_missing_files, version, save_results, save_results_name, raw_data_loc, key_location=key_location)
     plt_tools.plot_data_comparison(tim1, lat1, lon1, user_profile_alts, user_profile_values, tim2, lat2, lon2, dardar_profile_alts, dardar_profile_values, flight_separation_time=1, flight_separation_unit="m", colormap=colormap)
 
 
